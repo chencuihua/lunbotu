d5efab61e1b10c4fb537d013f78b90ea3b0b22d3
zhb <1019642861@qq.com>
2017-02-15 12:04:46 +0800

    轮播图基本功能完成

---
+++ js/index.js
@@ -0,0 +1,66 @@
+
+//先规定好每张图片处于的位置和状态
+var states = [
+                {ZIndex:1,width:120,height:150,top:69,left:134,ZOpacity:0.2},
+                {ZIndex:2,width:130,height:170,top:59,left:0,ZOpacity:0.5},
+                {ZIndex:3,width:170,height:218,top:35,left:110,ZOpacity:0.7},
+                {ZIndex:4,width:224,height:288,top:0,left:263,ZOpacity:1},
+                {ZIndex:3,width:170,height:218,top:35,left:470,ZOpacity:0.7},
+                {ZIndex:2,width:130,height:170,top:59,left:620,ZOpacity:0.5},
+                {ZIndex:1,width:120,height:150,top:69,left:500,ZOpacity:0.2}
+            ];
+
+var lis = $('#box li');
+//让每个 li 对应上面 states 的每一个状态
+function move(){
+	lis.each(function(index,ele){
+		var state = states[index];
+		$(ele).css('z-index',state.ZIndex).finish().animate(state,1000).find('img').css('opacity',state.ZOpacity);
+	});
+}
+//让 li 从正中间展开
+move();
+
+//点击下一张，让轮播图发生偏移
+function next(){
+	//原理:把数组最后一个元素移到数组第一位(以下等价)
+//	var obj = states.pop();  //删除最后一个元素并返回
+//	states.unshift(obj);     //将获得的元素插到最前面
+	states.unshift(states.pop());
+	move();
+}
+
+//点击上一张，让轮播图发生偏移
+function prev(){
+	//原理:把数组第一个元素移到数组最后一位(以下等价)
+//	var obj = states.shift();  //删除第一个元素并返回
+//	states.push(obj);     //将获得的元素插到最后面
+	states.push(states.shift());
+	move();
+}
+
+//点击下一张
+$('#box .next').click(function(){
+	next();
+});
+
+//点击上一张
+$('#box .prev').click(function(){
+	prev();
+});
+
+//自动播放
+var interval = null;
+function autoplay(){
+interval=setInterval(function(){
+	next();
+},2000);
+}
+autoplay();
+//鼠标悬停停止轮播
+$('#box li').add('#box section').hover(function(){
+	clearInterval(interval);
+},function(){
+	autoplay();
+});
+
